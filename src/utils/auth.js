26d2a7918b66ad5afef77efcee6431ec59abc0f5
Alfi Naim <alfons.devs@gmail.com>
2023-05-03 09:33:40 +0300

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 import api from "./MainApi";
 
-export const BASE_URL = "https://api.naim-news.students.nomoredomainssbs.ru";
-//export const BASE_URL = "http://localhost:3001";
+//export const BASE_URL = "https://api.naim-news.students.nomoredomainssbs.ru";
+export const BASE_URL = "http://localhost:3001";
 
 export const signin = ({ email, password }) => {
   return fetch(`${BASE_URL}/signin`, {
